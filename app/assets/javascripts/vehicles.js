b211406d3f5c7a0437b1820364006e352bfb3044
alunrhysbeynon <Alun@alun.local>
2018-04-13 15:35:50 +0100

    dummy database

---
+++ app/assets/javascripts/vehicles.js
@@ -0,0 +1,16 @@
+var vehicles = {
+
+    'Trailer 1': {
+        'VIN': 'EXAMPLEVIN001-M1',
+        'manufacturer': '',
+        'MVRIS': '123785',
+    },
+	
+	 'Trailer 2': {
+		'VIN': 'EXAMPLEVIN001-M1SP',
+		'SupplierCode': '1',
+		'MVRIS': '123785',
+		'Price': '£12,000.00',
+	},
+	
+};
