fbe853323eaac19332dc62369a08abf4c16f1a53
alunrhysbeynon <Alun@alun.local>
2018-04-20 09:57:06 +0100

    edited dummy database

--- app/assets/javascripts/vehicles.js
+++ app/assets/javascripts/vehicles.js
@@ -1,16 +1,21 @@
 var vehicles = {
 
-    'Trailer 1': {
-        'VIN': 'EXAMPLEVIN001-M1',
-        'manufacturer': '',
-        'MVRIS': '123785',
+    'A2B3 C4D5': {
+        'chassisNumber': 'EXAMPLEVIN001',
+        'manufacturer': 'M1 Engineering Ltd',
+        'maximumMass': '2,000',
+        'unlandenMass': '1,000',
+        'keeper': 'Waynes Haulage Group',
+        'address': '1 Example Street',
+        'email': 'wayne.example@wayneshaulagegroup.com',
+        'Key': '5555',
     },
 	
-	 'Trailer 2': {
-		'VIN': 'EXAMPLEVIN001-M1SP',
-		'SupplierCode': '1',
-		'MVRIS': '123785',
-		'Price': '£12,000.00',
-	},
+//	 'Trailer 2': {
+//		'VIN': 'EXAMPLEVIN001-M1SP',
+//		'SupplierCode': '1',
+//		'MVRIS': '123785',
+//		'Price': '£12,000.00',
+//	},
 	
 };
