dc712cc738699106348d66d6c5758144b359f497
Matt Davies <maffiew.davies@gmail.com>
2018-10-24 13:42:42 +0100

    Add search
    Add input and basic functions

---
+++ app/v6/assets/javascripts/search-within.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){
+    $('#searchWithin').keyup(function(){
+        var input = $('#searchWithin').val();
+        if ($('#searchWithin').val().length >= 3) {
+
+            var results = searchPartials(input, member.trailers);
+
+            if (results.length == 0) {
+                alert("no results found");
+            } else {
+                    var trailerRow = '<h2 class="heading-medium">' + results.length + ' trailer<span class="plural"></span></h2><table id="sortable-table"><thead><tr><th scope="col" onclick="sortTable(0)" id="reg" class="sort-default">Registration number</th><th scope="col" onclick="sortTable(1)" id="vin" class="sort-default"><abbr title="Vehicle Identification Number">VIN</abbr>/Chassis number</th><th scope="col" onclick="sortTable(2)" id="manu" class="sort-default">Manufacturer</th><th scope="col">&nbsp;</th></tr></thead><tbody>';
+            }
+    
+                for (i = results.length - 1; i >= 0; i--) {
+                    if (results[i].registered == true) {
+                        trailerRow += '<tr>';
+                        trailerRow += '<td scope="row"><span class="table-label">Registration</span><span class="table-details">' + results[i].reg + '</span><a href="#" class="manage-link" data="' + i + '">Manage</a></td>';
+                        if (!results[i].vin) {
+                        } else {
+                            trailerRow += '<td scope="row"><span class="table-label"><abbr title="Vehicle Identification Number">VIN</abbr></span><span class="table-details">' + results[i].vin + '</span></td>';
+                        }
+                        if (!results[i].chassis) {
+                        } else {
+                            trailerRow += '<td scope="row"><span class="table-label"><abbr title="Vehicle Identification Number">VIN</abbr>/Chassis</span><span class="table-details">' + results[i].chassis + '</span></td>';
+                        }
+                        trailerRow += '<td scope="row"><span class="table-label">Manufacturer</span><span class="table-details">' + results[i].manufacturer + '</span></td>';
+                        trailerRow += '<td scope="row" id="manage"><a href="#" class="manage" data="' + i + '">Manage</a></td>';
+                        // trailerRow += '<a href="#" class="manage-link" data="' + i + '">Manage</a>';
+                        trailerRow += '</tr>';
+                    }
+                }
+    
+                $('#trailer-table').html(trailerRow + '</tbody></table>');
+        }
+    })
+})
+
+
+/********************************************************************
+    Search each field, at any position, for each sub-string
+********************************************************************/
+function searchPartials(input, dataSource){
+//    e.preventDefault();
+    var items = dataSource;
+    var inputs = input.split(" ");
+    var holding = [];
+    var results = [];
+
+    for (var i = 0; i < items.length; i++) {
+        var counter = 0;
+        for (var s = 0; s < inputs.length; s++) {
+            if (items[i].reg.includes(inputs[s]) || items[i].vin.includes(inputs[s]) || items[i].manufacturer.includes(inputs[s])) {
+                counter++;
+                if (counter == inputs.length) {
+                    counter = 0;
+                    if (holding.includes(items[i])) {
+                    } else {
+                        holding.push(items[i]);
+                    }
+                }
+            }
+        }
+    }
+
+    for (var d = 0; d < holding.length; d++) {
+        if (results.includes(holding[d])) {
+        } else {
+            results.push(holding[d]);
+        }
+    }
+    return results;
+}
